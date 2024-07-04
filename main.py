458cc5bcffa66f747b23ed23a5c5efda9e30ab46
SakibSamiul <sb.sakib77@gmail.com>
2024-07-04 15:30:20 +0600

    Mad-libs Generators

---
+++ main.py
@@ -0,0 +1,15 @@
+name=(input("Enter an adjective: "))
+name1=(input("Enter an animal: "))
+name2=(input("Enter a verb: "))
+name3=(input("Enter a noun: "))
+name4=(input("Enter an animal: "))
+name5=(input("Enter an adjective: "))
+name6=(input("Enter a food: "))
+name7=(input("Enter a verb: "))
+name8=(input("Enter an animal: "))
+name9=(input("Enter an adjective: "))
+name10=(input("Enter a noun: "))
+
+output = f"Once upon a time, there was a {name} {name1} who loved to {name2} in the {name3}. Ond day, the {name4} discovered a {name5} {name6} and decided to {name7} it all up. Suddently, the {name8} felt {name9} and fell into a deep {name10}. The End."
+
+print(output)
